bb9de2091eb003f0e67909fb2ac65a916825541d
Myo Myat Min <myomyatmin01@gmail.com>
2022-01-12 20:41:41 +0630

    Print Hello World with php and html together

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php 
- echo 'Hello World';
+ // echo 'Hello World';
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+</head>
+<body>
+    <h1><?php echo 'Hello World';?></h1>
+</body>
+</html>
