d787e868f7d3d6a9f44f2dd2f472b3282fbdb74a
Myo Myat Min <myomyatmin01@gmail.com>
2022-01-13 15:30:50 +0630

    PHP String

--- index.php
+++ index.php
@@ -1,15 +1,41 @@
 <?php 
- // echo 'Hello World';
+
+    $stringOne = 'my email is ';
+    $stringTwo = 'mario123@thenetninja.co.uk';
+
+    echo $stringOne.$stringTwo;
+
+    $name = 'mario';
+
+    // concatenation
+    echo 'Hey, my name is '.$name;
+
+    // variable interpolation
+    echo "Hey, my name is $name"; 
+
+    // escaping characters
+    echo "the ninja screamed \"whaaa\"";
+    echo 'the ninja scream "whaaa"';
+
+    // get single characters
+    echo $name[1];
+
+    // string functions
+    echo strlen($name);
+    echo strtoupper($name);
+    echo strtolower($name);
+    echo str_replace('m', 'w', $name);
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title></title>
+    <title>PHP Tutorials</title>
 </head>
 <body>
-    <h1><?php echo 'Hello World';?></h1>
+
+    <p><?php echo $name; ?></p>
+
 </body>
 </html>
